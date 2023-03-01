1ae5a67b5e96181209df4c16992bf0cfc2b1250f
lova <lova_67@live.fr>
2023-03-01 09:49:28 +0300

    Connection with mysql ok

--- index.js
+++ index.js
@@ -1,53 +1,78 @@
 const express = require('express')
-const MongoClient = require('mongodb').MongoClient;
-const url ='mongodb+srv://startuplova:nwD1imRLYV6JYTuf@cluster0.kcxgiqe.mongodb.net/?retryWrites=true&w=majority'
 
+// test:TJUQ92IHuWVHncVY
 
 const app = express()
 
 const dbName = 'myApiBase';
 const collectionName = 'myCollectionAoi';
 
-const client = new MongoClient(url, { useNewUrlParser: true });
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'banyta1xrvb3umtoagof-mysql.services.clever-cloud.com',
+  user: 'ula4etzv4xwsbzue',
+  password: 'NMjQvyFMuxEKKNx5Rs5n',
+  database: 'banyta1xrvb3umtoagof'
+});
+
+
+
 
 
 app.use(express.json())
 
 const listener = app.listen(process.env.PORT, function() {
     console.log("Your app is listening on port " + listener.address().port)
-    const ip = request.connection.remoteAddress
-    console.log('Your IP address is: ${ip}')
   });
+
+app.get('/',(req,res)=>{
+
+  let base ='Not connected'
+  
+  console.log('I will send data to MariadDB');
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+    base = 'Connected';
+  });
+
+  const ipAddress = req.socket.remoteAddress;
+
+  res.send([{
+    ipAddress,
+    base    
+  }]);
+});
 
 app.put('/users/:id', (req, res) => {
   const updateCallBack = req.body
-  const date = Date.now();
-    res.send([
-        {updateCallBack,date}
-    ])
-    client.connect(err => {
-        if (err) {
-          console.error('Error connecting to MongoDB Atlas:', err);
-        } else {
-          console.log('Connected to MongoDB Atlas');
-      
-          const db = client.db(dbName);
-          const collection = db.collection(collectionName);
-      
-          // Insert a new document into the collection
-          const document = [ {updateCallBack,date}];
-          collection.insertOne(document, (err, result) => {
-            if (err) {
-              console.error('Error inserting document:', err);
-            } else {
-              console.log('Inserted document:', result.ops[0]);
-            }
-            client.close();
-          });
-        }
-      });
+  const correlationId = req.params.id;
+  
+  const now = Date.now(); // get current Unix timestamp in milliseconds
+  const date = new Date(now); // create new Date object from timestamp
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // months start at zero
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
 
-})
+  const dateString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
 
+  res.send([{
+      updateCallBack,
+      dateString,
+      correlationId
+    }]);
 
+const author = { dateReceived:dateString , objectID:JSON.stringify(updateCallBack), correlationId: correlationId  };
+
+connection.query('INSERT INTO authors SET ?', author, (err, res) => {
+  if(err) throw err;
+  console.log('Last insert ID:', res.insertId);
+});
+
+
+
+ })
